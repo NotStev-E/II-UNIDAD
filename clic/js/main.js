244fc16bb19abc3da9f4a7d738ec3262b21ceddc
Steve <stevegomitas3@gmail.com>
2022-05-13 17:45:39 -0600

    Nuevos archivos agregados

---
+++ clic/js/main.js
@@ -0,0 +1,11 @@
+
+// Se utiliza la palabra import y entre llaves
+// el nombre del objeto seguido de from y entre
+// comillas la direccion del archivo que lo contiene
+import { sumar } from "./operadores.js";
+import { restar } from "./operadores.js";
+
+// FUNCIÓN QUE SUMA DOS NÚMEROS
+
+sumar(10, 5)
+restar(78, 8);
---
+++ clic/js/operadores.js
@@ -0,0 +1,12 @@
+
+// La palabra export indica que otros archivos 
+// tienen acceso al código
+export function sumar(a, b){
+    var resultado = a + b;
+    console.log(resultado);
+}
+
+export function restar (a, b){
+    var resultado = a - b;
+    console.log(resultado);
+}
